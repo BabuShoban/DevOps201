bb80e806c633e89d06a2cb5f926c665aeaed28d0
Shoban Babu Manohar <ubuntu@ip-172-31-63-192.ec2.internal>
2019-10-14 12:56:00 +0000

    First commit

---
+++ src/main/java/com/automation/jenkins/JenkinsIntegration/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.automation.jenkins.JenkinsIntegration;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	@Test
+    public  void helloWorld( )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
